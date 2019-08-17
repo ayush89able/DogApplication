c8eae31088c49a90ba46eb862e74fe7d9a42e5e3
ayush89abe <ayush89abe@gmail.com>
2019-08-17 10:17:54 +0530

    path to base url of app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { FavDogComponent } from './fav-dog/fav-dog.component';
 
 
 const routes: Routes = [
+  { path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
   {
     path:'home',
     component:HomeComponent
